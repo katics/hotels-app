848b437fc220a7fc265661e570ccc6231b386383
Sasa Katic <sasa.katic@symphony.is>
2021-03-23 15:32:08 +0100

    initial commit for react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
 import ReactDOM from "react-dom";
+import { Router, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import "./index.scss";
 
 import AppPage from "./components/App";
 import { Provider } from "react-redux";
 import store from "./store";
+import HotelsPage from "./components/pages/hotels/Hotels";
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <AppPage />
-  </Provider>,
+  <Router history={history}>
+    <Provider store={store}>
+      <Route component={HotelsPage} />
+    </Provider>
+  </Router>,
+
   document.getElementById("root")
 );
